c2bbe2db736ec3b21f945faa4574b6a6cba5e106
Joel Thomas <wilddroppings@gmail.com>
2021-01-04 10:59:17 -0700

    add basic layout/style

---
+++ index.js
@@ -0,0 +1,36 @@
+var answers = ["It is certain",
+"It is decidedly so",
+"Without a doubt",
+"Yes - definitely",
+"You may rely on it",
+"As I see it, yes",
+"Most likely",
+"Outlook good",
+"Yes",
+"Signs point to yes",
+"Reply hazy, try again",
+"Ask again later",
+"Better not tell you now",
+"Cannot predict now",
+"Concentrate and ask again",
+"Don't count on it",
+"My reply is no",
+"My sources say no",
+"Outlook not so good",
+"Very doubtful"]
+
+var answerReveal = document.querySelector(".answer-reveal");
+var submitButton = document.querySelector(".submit");
+var question = document.querySelector(".question");
+var image = document.querySelector(".image1")
+submitButton.addEventListener('click', randomAnswer)
+
+function randomAnswer() {
+ answerReveal.value = answers[getRandomIndex(answers)]
+answerReveal.classList.remove("hidden")
+image.classList.add("hidden")
+}
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
